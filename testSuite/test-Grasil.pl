943ebf78189338a1cae3d300f28d1631d6588e68
Andrew Benson <abenson@carnegiescience.edu>
2019-09-30 09:06:46 +0000

    * Update parameter names.

--- testSuite/test-Grasil.pl
+++ testSuite/test-Grasil.pl
@@ -149,10 +149,9 @@ foreach my $outputRedshift ( $galacticus->{'outputs'}->{'redshift'}->list() ) {
 	    );
 	
 	# Extract recycling parameter for the model.
-	die("test-Grasil.pl: FAIL - fixed IMF and instantaneous recycling expected for this model")
-	    unless ( $galacticus->{'parameters'}->{'imfSelectionMethod'}->{'value'} eq "fixed" && $galacticus->{'parameters'}->{'stellarPopulationPropertiesMethod'}->{'value'} eq "instantaneous" );
-	my $imfRecycledAttributeName = "imf".$galacticus->{'parameters'}->{'imfSelectionFixed'}->{'value'}."RecycledInstantaneous";
-	my $recycledFraction = $galacticus->{'parameters'}->{$imfRecycledAttributeName}->{'value'};
+	die("test-Grasil.pl: FAIL - fixed stellar population and instantaneous recycling expected for this model")
+	    unless ( $galacticus->{'parameters'}->{'stellarPopulationSelectorMethod'}->{'value'} eq "fixed" && $galacticus->{'parameters'}->{'stellarPopulationPropertiesMethod'}->{'value'} eq "instantaneous" );
+	my $recycledFraction = $galacticus->{'parameters'}->{'stellarPopulationMethod'}->{'recycledFraction'}->{'value'};
 
 	# Determine the number of galaxies selected and write a message reporting this.
 	my $numberSelected = nelem($galacticus->{'selection'});
