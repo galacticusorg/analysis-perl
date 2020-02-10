a04bad6f806f86c44b5fbb1124a0ccb031036635
Andrew Benson <abenson@obs.carnegiescience.edu>
2020-02-10 11:52:51 -0800

    * Add derived properties for the total stellar and ISM metal masses.

--- perl/Galacticus/GasMass.pm
+++ perl/Galacticus/GasMass.pm
@@ -11,7 +11,8 @@ use Data::Dumper;
 use Galacticus::HDF5;
 
 %Galacticus::HDF5::galacticusFunctions = ( %Galacticus::HDF5::galacticusFunctions,
-    "massColdGas" => \&Galacticus::GasMass::Get_Cold_Gas_Mass,
+    "massColdGas"   => \&Galacticus::GasMass::Get_Cold_Gas_Mass  ,
+    "metalsColdGas" => \&Galacticus::GasMass::Get_Cold_Gas_Metals
     );
 
 sub Get_Cold_Gas_Mass {
@@ -28,6 +29,33 @@ sub Get_Cold_Gas_Mass {
 	if ( exists($model->{'dataSetsAvailable'}->{'diskMassGas'    }) );
     push(@gasMassComponents,"spheroidMassGas")
 	if ( exists($model->{'dataSetsAvailable'}->{'spheroidMassGas'}) );
+    push(@dataSetsRequired,@gasMassComponents);
+
+    # Get the datasets.
+    &Galacticus::HDF5::Get_Dataset($model,\@dataSetsRequired);
+
+    # Sum the gas masses.
+    $model->{'dataSets'}->{$dataSetName} = pdl zeroes(nelem($model->{'dataSets'}->{'mergerTreeWeight'}));
+    foreach my $component ( @gasMassComponents ) {
+	$model->{'dataSets'}->{$dataSetName} += $model->{'dataSets'}->{$component};
+    }
+
+}
+
+sub Get_Cold_Gas_Metals {
+    my $model       = shift;
+    my $dataSetName = $_[0];
+
+    # Get available datasets.
+    &Galacticus::HDF5::Get_Datasets_Available($model);
+
+    # Decide which datasets to get.
+    my @dataSetsRequired = ( "mergerTreeWeight" );
+    my @gasMassComponents;
+    push(@gasMassComponents,"diskAbundancesGasMetals"    )
+	if ( exists($model->{'dataSetsAvailable'}->{'diskAbundancesGasMetals'    }) );
+    push(@gasMassComponents,"spheroidAbundancesGasMetals")
+	if ( exists($model->{'dataSetsAvailable'}->{'spheroidAbundancesGasMetals'}) );
     push(@dataSetsRequired,@gasMassComponents);
 
     # Get the datasets.
--- perl/Galacticus/StellarMass.pm
+++ perl/Galacticus/StellarMass.pm
@@ -11,6 +11,7 @@ use Data::Dumper;
 
 %Galacticus::HDF5::galacticusFunctions = ( %Galacticus::HDF5::galacticusFunctions,
     "massStellar"       => \&Galacticus::StellarMass::Get_StellarMass      ,
+    "metalsStellar"     => \&Galacticus::StellarMass::Get_StellarMetals    ,
     "starFormationRate" => \&Galacticus::StellarMass::Get_StarFormationRate
     );
 
@@ -28,6 +29,33 @@ sub Get_StellarMass {
 	if ( exists($model->{'dataSetsAvailable'}->{'diskMassStellar'    }) );
     push(@stellarMassComponents,"spheroidMassStellar")
 	if ( exists($model->{'dataSetsAvailable'}->{'spheroidMassStellar'}) );
+    push(@dataSetsRequired,@stellarMassComponents);
+
+    # Get the datasets.
+    &Galacticus::HDF5::Get_Dataset($model,\@dataSetsRequired);
+
+    # Sum the stellar masses.
+    $model->{'dataSets'}->{$dataSetName} = pdl zeroes(nelem($model->{'dataSets'}->{'mergerTreeWeight'}));
+    foreach my $component ( @stellarMassComponents ) {
+	$model->{'dataSets'}->{$dataSetName} += $model->{'dataSets'}->{$component};
+    }
+
+}
+
+sub Get_StellarMetals {
+    my $model       = shift;
+    my $dataSetName = $_[0];
+
+    # Get available datasets.
+    &Galacticus::HDF5::Get_Datasets_Available($model);
+
+    # Decide which datasets to get.
+    my @dataSetsRequired = ( "mergerTreeWeight" );
+    my @stellarMassComponents;
+    push(@stellarMassComponents,"diskAbundancesStellarMetals"    )
+	if ( exists($model->{'dataSetsAvailable'}->{'diskAbundancesStellarMetals'    }) );
+    push(@stellarMassComponents,"spheroidAbundancesStellarMetals")
+	if ( exists($model->{'dataSetsAvailable'}->{'spheroidAbundancesStellarMetals'}) );
     push(@dataSetsRequired,@stellarMassComponents);
 
     # Get the datasets.
